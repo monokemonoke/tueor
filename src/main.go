c72562c506e9cbaa52dc23c19d254181b234f1ff
monokemonoke <kawabataryuhei@gmail.com>
2023-05-18 23:36:58 +0900

    :recycle: Goのソースコードとその他で生成方法を変える

--- src/main.go
+++ src/main.go
@@ -4,7 +4,9 @@ import (
 	"bytes"
 	"embed"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"go/format"
 	"html/template"
 	"io/fs"
 	"log"
@@ -38,33 +40,42 @@ func main() {
 }
 
 func readEntries(fsys fs.FS, dirname string, params map[string]map[string]interface{}) error {
-	fmt.Println("A", fsys, dirname)
 	entries, err := fs.ReadDir(fsys, dirname)
 	if err != nil {
-		fmt.Println("D", err)
 		return err
 	}
-	fmt.Println("B", len(entries))
 
 	for _, entry := range entries {
 		filename := dirname + "/" + entry.Name()
 		if filename[:2] == "./" {
 			filename = filename[2:]
 		}
-		fmt.Println("C", filename)
 
 		if entry.IsDir() {
 			readEntries(fsys, filename, params)
 		} else {
 			t := template.Must(template.ParseFS(fsys, filename))
 			param := params[filename]
-			generate(t, param, filename[:len(filename)-4])
+			generate(t, param, filename)
 		}
 	}
 	return nil
 }
 
-func generate(t *template.Template, params interface{}, filename string) {
+func generate(t *template.Template, params interface{}, filename string) error {
+	if len(filename) < 4 {
+		return errors.New("filename is too short")
+	}
+	generateFile := filename[:len(filename)-4]
+	ext := path.Ext(generateFile)
+
+	if ext == "go" {
+		return generateGo(t, params, generateFile)
+	}
+	return generateText(t, params, generateFile)
+}
+
+func generateText(t *template.Template, params interface{}, filename string) error {
 	var buf bytes.Buffer
 	t.Execute(&buf, params)
 
@@ -73,14 +84,42 @@ func generate(t *template.Template, params interface{}, filename string) {
 
 	nowDir, err := os.Getwd()
 	if err != nil {
-		log.Fatalln(err)
+		return err
 	}
 
 	if err := os.MkdirAll(path.Dir(path.Join(nowDir, filename)), 0777); err != nil {
-		log.Fatalln(err)
+		return err
 	}
 
 	if err := os.WriteFile(path.Join(nowDir, filename), out.Bytes(), 0644); err != nil {
-		log.Fatalln(err)
+		return err
 	}
+	return nil
+}
+
+func generateGo(t *template.Template, params interface{}, filename string) error {
+	var buf bytes.Buffer
+	t.Execute(&buf, params)
+
+	var out bytes.Buffer
+	out.Write(buf.Bytes())
+
+	body, err := format.Source(out.Bytes())
+	if err != nil {
+		return err
+	}
+
+	nowDir, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	if err := os.MkdirAll(path.Dir(path.Join(nowDir, filename)), 0777); err != nil {
+		return err
+	}
+
+	if err := os.WriteFile(path.Join(nowDir, filename), body, 0644); err != nil {
+		return err
+	}
+	return nil
 }
